41368f3b73295f65c9b9ddd11e5e515698ae1f71
kihno <stistyle13@gmail.com>
2022-09-16 12:33:28 -0700

    Create genre update get and post methods

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -125,9 +125,39 @@ exports.genre_delete_post = (req, res, next) => {
 };
 
 exports.genre_update_get = (req, res, next) => {
-    res.send('NOT IMPLEMENTED: Genre update GET');
+    Genre.findById(req.params.id).exec((err, genre) => {
+        if (err) { return next(err) }
+
+        if (genre == null) {
+            const err = new Error('Genre not found.');
+            err.status = 404;
+            return next(err);
+        }
+
+        res.render('genre_form', { title: 'Update Genre', genre });
+    });
 };
 
-exports.genre_update_post = (req, res, next) => {
-    res.send('NOT IMPLEMENTED: Genre update POST');
-};
+exports.genre_update_post = [
+    body('name', 'Genre name required')
+    .trim() 
+    .isLength({ min:1 })
+    .escape(),
+
+    (req, res, next) => {
+        const errors = validationResult(req);
+
+        const genre = new Genre({ name: req.body.name, _id: req.params.id });
+
+        if (!errors.isEmpty()) {
+            res.render('genre_form', { title: 'Update Genre', genre, errors: errors.array() });
+            return;
+        } 
+
+        Genre.findByIdAndUpdate(req.params.id, genre, {}, (err, thegenre) => {
+            if (err) { return next(err) }
+
+            res.redirect(thegenre.url);
+        });
+    }
+];
